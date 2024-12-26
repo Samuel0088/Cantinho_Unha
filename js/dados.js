93a66686f9f9cfdc057c703fd1b97f8d86fd33ee
Samuel Vieira <161070119+Samuel0088@users.noreply.github.com>
2024-12-26 11:29:41 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,176 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 01",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 5.00,
+            "link": "https://exemplo.com/adesivo1"
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "img/adesivo2.jpg",
+            "name": "Adesivo 02",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 03",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 5.00
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 04",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 5.00
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 05",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 06",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 5.00
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 07",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 5.00
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 08",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 5.00
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 09",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 10",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 5.00
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 11",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 5.00
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 12",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 5.00
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 01",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 5.00
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 02",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 03",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 5.00
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 04",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 5.00
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 05",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 06",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 07",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 08",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 09",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 10",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 5.00
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 11",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 5.00
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "img/adesivo1.jpg",
+            "name": "Adesivo 12",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 5.00
+        }
+    ]
+}
+
